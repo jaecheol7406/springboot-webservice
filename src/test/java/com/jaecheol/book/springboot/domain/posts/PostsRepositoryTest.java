816c340580195519a968e1dc54a40c9e7f8135eb
jaecheol7406 <57355410+jaecheol7406@users.noreply.github.com>
2022-01-10 21:24:04 +0900

    domain 추가 (Entity클래스, JPA Repository)

---
+++ src/test/java/com/jaecheol/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.jaecheol.book.springboot.domain.posts;
+
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void postSaveFindAll(){
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("wocjf8888")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
