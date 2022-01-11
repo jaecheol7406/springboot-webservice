7282042d9e592abbe85c1466399090efa3c0384a
jaecheol7406 <57355410+jaecheol7406@users.noreply.github.com>
2022-01-11 23:55:10 +0900

    PostsApiController

---
+++ src/main/java/com/jaecheol/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.jaecheol.book.springboot.service.posts;
+
+import com.jaecheol.book.springboot.domain.posts.PostsRepository;
+import com.jaecheol.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
