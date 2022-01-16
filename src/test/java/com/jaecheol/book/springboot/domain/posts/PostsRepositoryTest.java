6e9596420bfbad3c10a81ebe848b00d746e1e977
jaecheol7406 <57355410+jaecheol7406@users.noreply.github.com>
2022-01-16 14:48:56 +0900

    JPA Auditing 적용 및 테스트

--- src/test/java/com/jaecheol/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jaecheol/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,15 +1,15 @@
 package com.jaecheol.book.springboot.domain.posts;
 
-import org.aspectj.lang.annotation.After;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
@@ -43,5 +43,27 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_save() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2022, 1, 16, 0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(posts.getCreatedDate() + "   " + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
