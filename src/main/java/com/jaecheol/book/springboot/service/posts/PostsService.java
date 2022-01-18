0d9844d67192604b33a1a6f1e98c044e1e959baa
jaecheol7406 <57355410+jaecheol7406@users.noreply.github.com>
2022-01-18 23:34:10 +0900

    전체 조회 화면 추가

--- src/main/java/com/jaecheol/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jaecheol/book/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.jaecheol.book.springboot.service.posts;
 
 import com.jaecheol.book.springboot.domain.posts.Posts;
 import com.jaecheol.book.springboot.domain.posts.PostsRepository;
+import com.jaecheol.book.springboot.web.dto.PostsListResponseDto;
 import com.jaecheol.book.springboot.web.dto.PostsResponseDto;
 import com.jaecheol.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jaecheol.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +33,12 @@ public class PostsService {
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
